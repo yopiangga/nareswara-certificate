5abfb40d116685713df50958a552effae9b57d66
yopiangga <yopigambyok@gmail.com>
2022-11-14 20:16:32 +0700

    server next js | add

---
+++ server/pages/api/user/delete.js
@@ -0,0 +1,26 @@
+import { ObjectId } from "mongodb";
+import { initDB } from "service/db";
+import NextCors from "nextjs-cors";
+
+export default async function handler(req, res) {
+  const db = await initDB();
+  await NextCors(req, res, {
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  });
+
+  if (req.method === "DELETE") {
+    try {
+      const result = await db
+        .collection("users")
+        .deleteOne({ _id: ObjectId(req.body._id) });
+
+      return res.status(200).json({ message: "Berhasil delete", data: result });
+    } catch (e) {
+      return e;
+    }
+  } else {
+    return res.status(200).json(null);
+  }
+}
---
+++ server/pages/api/user/edit.js
@@ -0,0 +1,31 @@
+import { ObjectId } from "mongodb";
+import { initDB } from "service/db";
+import NextCors from "nextjs-cors";
+
+export default async function handler(req, res) {
+  const db = await initDB();
+  await NextCors(req, res, {
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  });
+
+  if (req.method === "POST") {
+    const data = {
+      ...req.body.data,
+      updatedAt: new Date(),
+    };
+
+    try {
+      const result = await db
+        .collection("users")
+        .updateOne({ _id: ObjectId(req.body._id) }, { $set: data });
+
+      return res.status(200).json({ message: "Berhasil update", data: result });
+    } catch (e) {
+      return e;
+    }
+  } else {
+    return res.status(200).json(null);
+  }
+}
