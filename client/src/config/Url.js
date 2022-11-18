80d984cac93cc39e868142a6e68a892ca147d49e
yopiangga <yopigambyok@gmail.com>
2022-11-18 14:13:11 +0700

    Build Application, Client & Server

--- client/src/config/Url.js
+++ client/src/config/Url.js
@@ -1,5 +1,5 @@
-// const baseUrl = "https://pregnant-app-server-next.vercel.app/api";
-const baseUrl = "http://192.168.43.119:3000/api";
+const baseUrl = "https://nareswara-certificate-server.vercel.app/api";
+// const baseUrl = "http://192.168.43.119:3000/api";
 // const baseUrl = "http://localhost:3000/api"
 // const baseUrl = "http://10.252.130.118:3000/api";
 
