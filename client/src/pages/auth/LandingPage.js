4629b9f2b8c8b2c20ab3cb9db14ffaf569dc348b
yopiangga <yopigambyok@gmail.com>
2022-11-18 10:47:42 +0700

    mobile verify

--- client/src/pages/auth/LandingPage.js
+++ client/src/pages/auth/LandingPage.js
@@ -1,95 +1,14 @@
-import logo from "../../logo.svg";
 import onlineTest from "../../assets/images/online_test.svg";
 import { Link } from "react-router-dom";
+import { NavbarLanding } from "src/components/NavbarComponent";
+import { FooterComponent } from "src/components/FooterComponent";
 
 export default function LandingPage() {
   return (
     <div>
-      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-        <div className="container flex flex-wrap justify-between items-center mx-auto">
-          <a href="/" className="flex items-center">
-            <img src={logo} className="mr-3 h-6 sm:h-9" alt="Nares Logo" />
-            <div className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              <h2 className="">
-                Nares <span className="text-indigo-600">Certificate</span>
-              </h2>
-            </div>
-          </a>
+      <NavbarLanding />
 
-          <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                ffillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="flex flex-col p-4 mt-4 items-center bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pr-4 pl-3 text-base text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                  aria-current="page"
-                >
-                  Solusi
-                </a>
-              </li>
-
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pr-4 pl-3 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Produk
-                </a>
-              </li>
-
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pr-4 pl-3 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Mitra
-                </a>
-              </li>
-
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pr-4 pl-3 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Tentang Kami
-                </a>
-              </li>
-
-              <li>
-                <Link to="/login">
-                  <a className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                    Masuk
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <section>
+      <section className="pt-20">
         <div className="px-4 py-16 mx-auto max-w-screen-xl sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 gap-8 lg:gap-16 lg:grid-cols-2">
             <div className="relative h-64 rounded-lg sm:h-80 lg:h-full lg:order-last">
@@ -107,50 +26,46 @@ export default function LandingPage() {
                 cepat dan terjamin keasliannya.
               </p>
 
-              <Link to="/register">
-                <a className="inline-flex items-center px-8 py-3 mt-8 text-white bg-indigo-600 border border-indigo-600 rounded hover:bg-transparent hover:text-indigo-600 active:text-indigo-500 focus:outline-none focus:ring">
-                  <span className="text-sm font-medium"> Mulai Sekarang </span>
+              <div className="flex lg:flex-row flex-col gap-2 mt-8">
+                <Link to="/register">
+                  <a className="inline-flex items-center px-8 py-3 text-white bg-indigo-600 border border-indigo-600 rounded hover:bg-transparent hover:text-indigo-600 active:text-indigo-500 focus:outline-none focus:ring">
+                    <span className="text-sm font-medium">
+                      {" "}
+                      Mulai Sekarang{" "}
+                    </span>
 
-                  <svg
-                    className="w-5 h-5 ml-3"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLineJoin="round"
-                      strokeWidth="2"
-                      d="M17 8l4 4m0 0l-4 4m4-4H3"
-                    />
-                  </svg>
-                </a>
-              </Link>
+                    <svg
+                      className="w-5 h-5 ml-3"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLineJoin="round"
+                        strokeWidth="2"
+                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                      />
+                    </svg>
+                  </a>
+                </Link>
+
+                <Link to="/verify">
+                  <a className="inline-flex items-center px-8 py-3 text-gray-600 bg-gray-200 border border-gray-200 rounded hover:border-indigo-600 hover:bg-transparent hover:text-indigo-600 active:text-gray-100 focus:outline-none focus:ring">
+                    <span className="text-sm font-medium">
+                      {" "}
+                      Cek Sertifikat{" "}
+                    </span>
+                  </a>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </section>
 
-      <footer className="text-gray-600 body-font">
-        <div className="container py-8 mx-auto flex items-center sm:flex-row flex-col">
-          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <img className="mr-3 h-6 sm:h-9" src={logo} />
-            <span className="ml-3 text-lg">Nares Certificate</span>
-          </a>
-          <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-            © 2022 — Tim Nareswara
-          </p>
-          <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-            <a className="text-gray-500" href="https://pens.ac.id">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/154/154843.png"
-                className="h-4 sm:h-5"
-              />
-            </a>
-          </span>
-        </div>
-      </footer>
+      <FooterComponent />
     </div>
   );
 }
--- client/src/pages/auth/VerifyPage.js
+++ client/src/pages/auth/VerifyPage.js
@@ -2,7 +2,8 @@ import React, { useState, useContext } from "react";
 
 import { ModalInformationLittle } from "../../components/ModalInformationComponent";
 import { SmartContractContext } from "src/context/SmartContractContext";
-
+import { NavbarLanding } from "src/components/NavbarComponent";
+import { FooterComponent } from "src/components/FooterComponent";
 
 export default function VerifyPage() {
   const [data, setData] = useState({ address: "", cid: "" });
@@ -40,81 +41,64 @@ export default function VerifyPage() {
 
   return (
     <>
-    <ModalInformationLittle
+      <ModalInformationLittle
         status={modalInformationLittle.status}
         title={modalInformationLittle.title}
         description={modalInformationLittle.description}
         handleClose={handleCloseModal}
       />
-    <div className="flex flex-col items-center justify-center mx-auto md:h-screen ">
-      <div id="revue-embed" className="mb-4 max-w p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-          <div>
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Email anda
-            </label>
-            <input
-              type="text"
-              name="address"
-              id="email"
-              onChange={handleChange}
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Meta ID"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="password"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Kata sandi
-            </label>
-            <input
-              type="text"
-              name="cid"
-              onChange={handleChange}
-              id="password"
-              placeholder="CID"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            {/* <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="remember"
-                        aria-describedby="remember"
-                        type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                        required
-                      />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label
-                        htmlFor="remember"
-                        className="text-gray-500 dark:text-gray-300"
-                      >
-                        Remember me
-                      </label>
-                    </div>
-                  </div> */}
-          </div>
-          <button
-            type="submit"
-            className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-          >
-            Check
-          </button>
-        </form>
+      <NavbarLanding />
+      <div className="flex flex-col items-center justify-center mx-auto md:h-screen md:py-0 py-10">
+        <div
+          id="revue-embed"
+          className="mb-4 w-11/12 md:w-96 p-6 md:mt-0 mt-16 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
+        >
+          <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Email anda
+              </label>
+              <input
+                type="text"
+                name="address"
+                id="email"
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Meta ID"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Kata sandi
+              </label>
+              <input
+                type="text"
+                name="cid"
+                onChange={handleChange}
+                id="password"
+                placeholder="CID"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+              />
+            </div>
 
-        
+            <button
+              type="submit"
+              className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Check
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+      <FooterComponent />
     </>
   );
 }
